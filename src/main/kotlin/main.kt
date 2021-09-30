b03b122872b993995036e80b39d861921410749c
Gleb Ingman <igr2309@mail.ru>
2021-10-01 01:57:11 +0300

    Made a prototype

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,31 @@
-fun main(args: Array<String>) {
+enum class QueryType { ADD, REMOVE, GET }
+
+data class Element(val key: String, val value: String?)
+
+data class InputData(val queryType: QueryType, val element: Element)
+
+fun processInput(args: Array<String>): InputData {
     TODO()
+}
+
+fun add(element: Element) {
+    TODO()
+}
+
+fun remove(element: Element) {
+    TODO()
+}
+
+fun getValue(element: Element): String? {
+    TODO()
+}
+
+fun main(args: Array<String>) {
+    val (queryType, element) = processInput(args)
+
+    when (queryType) {
+        QueryType.ADD -> add(element)
+        QueryType.REMOVE -> remove(element)
+        QueryType.GET -> println(getValue(element))
+    }
 }
