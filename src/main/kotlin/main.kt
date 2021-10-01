bcfac279d40282c7bc6c28fc62313381ab5dadbd
Gleb Ingman <igr2309@mail.ru>
2021-10-02 00:23:04 +0300

    Made function getNode and tests for it

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,14 @@
+import java.io.File
+import java.lang.Integer.toBinaryString
+
+const val HASHLEN = 31
+const val STARTDIR = "db"
+
 enum class QueryType { ADD, REMOVE, GET }
 
-data class Element(val key: String, val value: String?)
+data class Element(val key: String, val value: String?) {
+    val keyHash = toBinaryString(key.hashCode()).padStart(HASHLEN, '0')
+}
 
 data class InputData(val queryType: QueryType, val element: Element)
 
@@ -16,9 +24,73 @@ fun remove(element: Element) {
     TODO()
 }
 
-fun getValue(element: Element): String? {
-    TODO()
+/*
+ * Получает element.
+ * Ищет файл с ключом element.key спуском по структуре.
+ * Если файл найден, то возращает его, иначе null.
+ */
+fun getNode(element: Element): File? {
+    // начинаем в корне структуры
+    var currDir = File(STARTDIR)
+
+    for (i in 0 until HASHLEN) {
+        when {
+            // пытаемся перейти по биту
+            File(currDir, element.keyHash[i].toString()).isDirectory -> {
+                currDir = File(currDir, element.keyHash[i].toString())
+            }
+            // пытаемся перейти по суффиксу
+            File(currDir, element.keyHash.drop(i)).isDirectory -> {
+                currDir = File(currDir, element.keyHash.drop(i))
+                break
+            }
+            else -> return null
+        }
+    }
+
+    // обходим файлы в поиске нашего ключа
+    currDir.walk().forEach {
+        if (it.isFile && getKeyFromFile(it) == element.key) {
+            return it
+        }
+    }
+    return null
 }
+
+/*
+ * В директории parent создаёт файл для element и возращает его
+ */
+fun createFileForElement(parent: File, element: Element): File {
+
+    /*
+     * Находит МЕХ имён файлов в папке parent
+     */
+    fun mex(): Int {
+        var i = 0
+        while (File(parent, i.toString()).exists()) ++i
+        return i
+    }
+
+    val file = File(parent, mex().toString())
+    file.createNewFile()
+    file.writeText("${element.key}\n${element.value}\n")
+    return file
+}
+
+fun clearDatabase() {
+    File(STARTDIR).deleteRecursively()
+    File(STARTDIR).mkdir()
+}
+
+/*
+ * По файлу получает ключ, который в нем хранится
+ */
+fun getKeyFromFile(file: File) = file.readLines()[0]
+
+/*
+ * По файлу получает значение, которое в нем хранится
+ */
+fun getValueFromFile(file: File) = file.readLines()[1]
 
 fun main(args: Array<String>) {
     val (queryType, element) = processInput(args)
@@ -26,6 +98,9 @@ fun main(args: Array<String>) {
     when (queryType) {
         QueryType.ADD -> add(element)
         QueryType.REMOVE -> remove(element)
-        QueryType.GET -> println(getValue(element))
+        QueryType.GET -> {
+            val node = getNode(element)
+            println(if (node == null) "No such key" else getValueFromFile(node))
+        }
     }
 }
--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,9 +1,42 @@
+import java.io.File
 import kotlin.test.*
 
 internal class Test1 {
 
+    @BeforeTest
+    fun setUp() {
+        clearDatabase()
+    }
+
+    @AfterTest
+    fun tearDown() {
+        clearDatabase()
+    }
+
     @Test
-    fun test1() {
-        assert(true)
+    fun testGetNode1() {
+        val elem = Element("some_key", "some_value")
+        val parent = File("db/${elem.keyHash}")
+        parent.mkdirs()
+        val file = createFileForElement(parent, elem)
+
+        assertEquals(getNode(elem), file)
+    }
+
+    @Test
+    fun testGetNode2() {
+        val elem1 = Element("one_key", "one_value")
+        val parent1 = File("db/${elem1.keyHash[0]}/${elem1.keyHash.drop(1)}")
+        parent1.mkdirs()
+        val file1 = createFileForElement(parent1, elem1)
+
+        val elem2 = Element("other_key", "other_value")
+        val parent2 = File("db/${elem2.keyHash[0]}/${elem2.keyHash.drop(1)}")
+        parent2.mkdirs()
+        val file2 = createFileForElement(parent2, elem2)
+
+        assertEquals(getNode(elem1), file1)
+        assertEquals(getNode(elem2), file2)
+        assertEquals(getNode(Element("third_key", null)), null)
     }
 }
